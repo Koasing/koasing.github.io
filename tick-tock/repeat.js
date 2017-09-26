8d9db85b348783fc433068361815303bc00289ea
Koasing <koasing@gmail.com>
2017-09-26 16:23:35 +0900

    Refine code readibility

--- tick-tock/repeat.js
+++ tick-tock/repeat.js
@@ -3,6 +3,7 @@
 var tick = document.getElementById("audio_tick");
 var tock = document.getElementById("audio_tock");
 
+var bpm    = 120;
 var period = 500;
 var beats  = 4;
 
@@ -10,7 +11,7 @@ var timer = null;
 
 function play(c)
 {
-    // timer is null iff not running
+    // timer exists if and only if loop is running.
     if(timer) {
         if(!c) tick.play(); else tock.play();
         timer = setTimeout(play, period, (c+1)%beats);
@@ -19,12 +20,11 @@ function play(c)
 
 function toggle()
 {
-    var bpm = parseInt(document.getElementById("bpm").value);
+    bpm = parseInt(document.getElementById("bpm").value);
     if(bpm < 1 || 360 < bpm) {
         alert("BPM out of range!")
         return false;
     }
-    period = 60000 / bpm;
 
     beats = parseInt(document.getElementById("beats").value);
     if(beats < 1 || 36 < beats) {
@@ -32,13 +32,16 @@ function toggle()
         return false;
     }
 
+    // 1 minute = 60 seconds =  60 * 1000 ms
+    period = 60000 / bpm;
+    
     if(timer) {
-        // timer exists, currently running
+        // timer exists; stop loop.
         clearTimeout(timer); timer = null;
         document.getElementById("play").innerHTML = "Play!";
     }
     else {
-        // timer not exist, start toggling after 100 ms
+        // timer not exist; start loop after 100 ms
         timer = setTimeout(play, 100, 0);
         document.getElementById("play").innerHTML = "Stop!";
     }
